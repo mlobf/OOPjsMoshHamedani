1dcb477ff8bce8dcad76f6188b741368d3cf9459
marcos <marcoslemeborbafilho@gmail.com>
2020-10-22 14:11:45 -0300

    Objects

---
+++ sectionTwo-Objects/objectsLiterals.js
@@ -0,0 +1,17 @@
+// Its a simple way to define an object.
+// Use let or const
+// const = constant, cant reassign .
+// Otherwise, let.
+
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    draw: function() {
+        console.log('draw');
+    }
+};
+
+circle.draw();
