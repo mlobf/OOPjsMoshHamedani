90b9807986b351d67f0499ebb7271744afdb0fcf
marcos <marcoslemeborbafilho@gmail.com>
2020-11-03 12:08:46 -0300

    lesson 14 enumerate properties

--- sectionTwo-Objects/addRemovingProperties.js
+++ sectionTwo-Objects/addRemovingProperties.js
@@ -1,9 +1,29 @@
 console.log('--------------------------------');
+/*
+    The objects in JS are dynamic, 
+    you can add properties or delete them.
+    Why this is usefull?
+        A site need a client's token....this new token is a new property.
+*/
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function(){
+        console.log('draw');
+    }
+}
 
+// To add a new property
+const circle = new Circle(10);
 
+circle.location = { x: 1};//1 way to do it.
+circle['locatium'] = {x: 1};//2 way to do it.
+// This second is better when we have special caracters as 
+// dfasdfasdf-adsfasdf or spaces between caracters.
 
+// To delete we user .delete
 
-
+delete circle.locatium;
+delete circle['location'];
 
 
 
