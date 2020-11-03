c4723488989d8e2221a6b3486c33bcff129ed052
marcos <marcoslemeborbafilho@gmail.com>
2020-11-03 17:12:50 -0300

    Learning is good!!

--- prototypes/classicalXprototype.js
+++ prototypes/classicalXprototype.js
@@ -1,1 +1,23 @@
 // Lets show the diferences between .
+// In JS we dont have classes, only objects.
+// so how to implente Inheritance in objects?
+//
+
+let x = { };
+let y = { };
+
+let z = Object.getPrototypeOf(x);
+
+console.log(z);
+
+let result = Object.getPrototypeOf(x)=== Object.getPrototypeOf(y);
+
+console.log(result);
+
+
+//How does JS works?
+// First will looks to  own objects prototypes, if didnt find it, goes upper, 
+//      climbing parent Classes until find 
+//      something.
+
+console.log('Protype is just a regular object on memory !!');
---
+++ prototypes/multilevelInheritance.js
@@ -0,0 +1,5 @@
+// Lets find out
+let myArray = [];
+
+// MyArray derives from arrayBase and arrayBase derives from objectBase.
+
