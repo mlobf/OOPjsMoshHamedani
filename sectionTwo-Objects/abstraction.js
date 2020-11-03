8f38444bff2cb84efa2cd580478f102cf971c947
marcos <marcoslemeborbafilho@gmail.com>
2020-11-03 13:19:37 -0300

    Lesson 17 getter and setters

---
+++ sectionTwo-Objects/abstraction.js
@@ -0,0 +1,20 @@
+console.log('========= Lesson 15 , Abstraction ============');
+// Abstraction Means..... hide the details, show the essencials.
+
+function Circle(radius) {
+    this.radius = radius;
+
+    this.defaultLocation = { x:0, y:0};
+
+    this.computeOptimumLocation = function(factor){
+        //... 
+    }
+
+    this.draw = function(){
+        this.computeOptimumLocation(0.1);
+        console.log('draw');
+    }
+}
+
+const circle = new Circle(10);
+circle.draw();
