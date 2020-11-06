59a79616ca274a928df79a216835022840d9ee0b
marcos <marcoslemeborbafilho@gmail.com>
2020-11-06 09:21:22 -0300

    End of this sesson

--- prototypeInheritance/exercise.js
+++ prototypeInheritance/exercise.js
@@ -1,32 +1,63 @@
 //Lets some exercises .... 
 console.log('----------------- Lesson 41/42 Exercise 1 --------------------');
 
+
+// The constructor function
 function HtmlElement() {
     this.click = function () {
         console.log('clicked');
     }
 }
-
+//The parent object
 HtmlElement.prototype.focus = function () {
     console.log('focued');
 }
 
+// Another constructor
 function HtmlSelectElement(items = []) {
     this.items = items;
 
     this.addItem = function (item) {
         this.items.push(items);
     }
+
+
+    this.removeItems = function (items) {
+        this.items.splice(this.items.indexOf(item), 1);
+    }
+
+    this.render = function () {
+        return `
+    <select>${this.items.map(item => `
+    <option>${item}</option>`).join('')}
+    </select>`;
+    }
 }
 
-this.removeItems = function (items) {
-    this.items.splice(this.items.indexOf(item), 1);
-}
 
+
+//Setting the prototype of HtmlSelectElement to a instance of HtmlElement object
 HtmlSelectElement.prototype = new HtmlElement();
 
-
-
-
-
 console.log('----------------- Lesson 43/44 Exercise 2 --------------------');
+
+// Extend the HtmlSelectElement and Implement a Random Method.
+// This is an polymorphism exercise.
+
+
+
+
+
+
+
+
+function HtmlImageElement(src) {
+    this.src = src;
+
+    this.render = function () {
+        return ` < img src = "${this.src}" / > `;
+    }
+
+}
+HtmlImageElement.prototype = new HtmlElement();
+HtmlImageElement.prototype.constructor = HtmlImageElement;
