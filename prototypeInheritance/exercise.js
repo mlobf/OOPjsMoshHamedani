f3713bbd1cfda4feff8a2ad3eb62f16c615bdabe
marcos <marcoslemeborbafilho@gmail.com>
2020-11-05 23:35:50 -0300

    last commit for today....

--- prototypeInheritance/exercise.js
+++ prototypeInheritance/exercise.js
@@ -1,9 +1,32 @@
 //Lets some exercises .... 
 console.log('----------------- Lesson 41/42 Exercise 1 --------------------');
 
+function HtmlElement() {
+    this.click = function () {
+        console.log('clicked');
+    }
+}
+
+HtmlElement.prototype.focus = function () {
+    console.log('focued');
+}
+
+function HtmlSelectElement(items = []) {
+    this.items = items;
+
+    this.addItem = function (item) {
+        this.items.push(items);
+    }
+}
+
+this.removeItems = function (items) {
+    this.items.splice(this.items.indexOf(item), 1);
+}
+
+HtmlSelectElement.prototype = new HtmlElement();
+
+
 
 
 
 console.log('----------------- Lesson 43/44 Exercise 2 --------------------');
-
-
