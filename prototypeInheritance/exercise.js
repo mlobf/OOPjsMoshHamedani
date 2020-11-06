51b9a00e6f501304bdbc4eee8778ebe7062ee08a
marcos <marcoslemeborbafilho@gmail.com>
2020-11-05 23:24:51 -0300

    Exercise Prototypes

---
+++ prototypeInheritance/exercise.js
@@ -0,0 +1,9 @@
+//Lets some exercises .... 
+console.log('----------------- Lesson 41/42 Exercise 1 --------------------');
+
+
+
+
+console.log('----------------- Lesson 43/44 Exercise 2 --------------------');
+
+
